003e5bbc24be559c0bbbeff34007263cf6239884
Charlie Davis <charlie.davis@humanify.com>
2015-12-10 09:10:18 -0500

    Generate a deployable war file.

--- localization/src/main/java/com/humanify/localization/LocalizationApplication.java
+++ localization/src/main/java/com/humanify/localization/LocalizationApplication.java
@@ -1,23 +1,25 @@
 package com.humanify.localization;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import com.humanify.localization.service.LocalizationService;
 
 // TODO: publish update notifications on MQ bus
 
 
 @SpringBootApplication
-public class LocalizationApplication
+public class LocalizationApplication extends SpringBootServletInitializer
 {
 	
-	@Autowired
-	LocalizationService service;
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app)
+	{
+		return app.sources(LocalizationApplication.class);
+	}
 	
 	public static void main(String[] args)
 	{
